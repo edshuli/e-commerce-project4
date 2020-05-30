6af3d622d86fec68c5d3ec7847cd2d469de7fb94
Edna <edna_shuli@hotmail.gr>
2020-05-30 07:41:33 +0000

    add checkout.js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_xaz4J0PBAIKzVxjzmxMxHGuD00Vbpuk5HX',
+        'client_secret': 'test client secret',
+
     }
 
     return render(request, template, context)
