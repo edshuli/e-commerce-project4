1d4d5dd8e5bad549381cd6105c55df30f9a32f82
Edna <edna_shuli@hotmail.gr>
2020-06-03 07:11:33 +0000

    Deploy part 1

--- ecommerce_project4/settings.py
+++ ecommerce_project4/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,8 +116,7 @@ WSGI_APPLICATION = 'ecommerce_project4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+DATABASES = {'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
