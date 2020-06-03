ec2769fd680855d5add6db913c665b8138a4b5b7
Edna <edna_shuli@hotmail.gr>
2020-06-03 09:41:46 +0000

    changes

--- ecommerce_project4/settings.py
+++ ecommerce_project4/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecomproject4.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['ecomproject4.herokuapp.com', 'localhost']
 
 
 # Application definition
