6a2583c28c77fd7d7c2bcd195d1c61870791f30d
Edna <edna_shuli@hotmail.gr>
2020-06-09 12:47:20 +0000

    visual editiing

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,13 +11,13 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
+    for item_id, item_data in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
+        total += item_data * product.price
+        product_count += item_data
         bag_items.append({
             'item_id': item_id,
-            'quantity': quantity,
+            'quantity': item_data,
             'product': product,
         })
 
@@ -27,9 +27,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
