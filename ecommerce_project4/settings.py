0cd46c6197a4f5d154e8f06c36a23f608710dbe7
Edna <edna_shuli@hotmail.gr>
2020-06-10 15:06:07 +0000

    added new settings

--- ecommerce_project4/settings.py
+++ ecommerce_project4/settings.py
@@ -18,10 +18,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '@oj1nw+-1jskn1%of3%gn$e@1$t$d8j-p9b%_7m_ot74&@(o&9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['ecompro4.herokuapp.com', 'localhost']
 
